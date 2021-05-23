b7ccffee274e90a558250c7ad0acb5761e745ca8
Daniel-Amaya18 <62664190+Daniel-Amaya18@users.noreply.github.com>
2021-05-23 12:40:59 -0500

    Update bucaramangaSQL.py

--- Codigo Python/bucaramangaSQL.py
+++ Codigo Python/bucaramangaSQL.py
@@ -5,9 +5,36 @@ def VictimasPorGenero():
               group by sexo
               order by accidentes desc"""
 
-# Costos anuales por Eps              
+
+# Costos totales por Eps              
+def CostosPorEPS():
+    return """select e.nombre_eps, sum(acc.costos) as costo_total
+              from accidente_victima acc join eps e on (acc.id_eps = e.id)
+              group by e.nombre_eps
+              order by costo_total asc"""
+
+
+# Numero de accidentes por cada tipo de vehiculo
+def AccidentesPorVehiculo():
+    return """select tip.nombre, count(numero) as accidentes
+              from accidente_victima acc join tipo_vehiculo tip on (acc.id_tipo_vehiculo = tip.id)
+              group by tip.nombre
+              order by accidentes desc"""
+
+
+# Numero de accidentes por grupo_etario
+def AccidentesPorGrupo():
+    return """select acc.grupo_etario, count(numero) as accidentes
+              from accidente_victima acc
+              group by acc.grupo_etario
+              order by accidentes desc"""
+
+
+
+# Costos anuales por EPS
 def CostosAnualesPorEPS():
-    return """select e.nombre_eps, sum(acc.costos) as costo_total, costo_2018, costo_2019, costo_2020, costo_2021
+    return """select e.nombre_eps, sum(acc.costos) as costo_total, coalesce(costo_2018,0) as "2018", 
+              coalesce(costo_2019,0) as "2019", coalesce(costo_2020, 0) as "2020", coalesce(costo_2021,0) as "2021"
               from accidente_victima acc join eps e on (acc.id_eps = e.id)
               left join (select nombre_eps as eps_2018, sum(acc.costos) as costo_2018 
                          from accidente_victima acc join eps on (acc.id_eps = id)
@@ -26,10 +53,11 @@ def CostosAnualesPorEPS():
                          where acc.anio_accidente = 2021
                          group by eps_2021) as gasto_2021 on (e.nombre_eps = eps_2021)
               group by e.nombre_eps, costo_2018, costo_2019, costo_2020, costo_2021
-              order by costo_total desc"""
+			  order by costo_total asc"""
 
-# Numero de accidentes por cada tipo de vehiculo
-def AccidentesPorVehiculo():
+
+# Accidentes por vehiculo y genero
+def AccidentesVehiculoGenero():
     return """select tip.nombre, count(numero) as accidentes, hombres, mujeres
               from accidente_victima acc join tipo_vehiculo tip on (acc.id_tipo_vehiculo = tip.id)
               left join (select tip.nombre as vehiculo, count(acc.numero) as hombres
@@ -44,22 +72,37 @@ def AccidentesPorVehiculo():
               order by accidentes desc"""
 
 
-# Numero de accidentes por grupo_etario
-def AccidentesPorGrupo():
-    return """select tip.nombre, count(numero) as accidentes
-              from accidente_victima acc join tipo_vehiculo tip on (acc.id_tipo_vehiculo = tip.id)
-              group by tip.nombre
-              order by accidentes desc"""
+# Costos totales
+def CostosTotales():
+    return """select sum(costos)
+              from accidente_victima"""
 
 
+def NumeroAccidentes():
+    return """select count(numero)
+              from accidente_victima"""
+              
 
-
-
-
-
-
-
-
-
-
+# Accidentes por dia
+def AccidentesPorDia():
+    return """select acc.dia_semana_accidente as dia, count(acc.numero) as total_acc, num2018 as "2018", num2019 as "2019", num2020 as "2020", num2021 as "2021"
+              from accidente_victima acc
+              join (select acc.dia_semana_accidente as semana2018, count(acc.numero) as num2018 
+                    from accidente_victima acc
+                    where acc.anio_accidente = 2018
+                    group by semana2018) as acc_2018 on (acc.dia_semana_accidente = semana2018)
+              join (select acc.dia_semana_accidente as semana2019, count(acc.numero) as num2019 
+                    from accidente_victima acc
+                    where acc.anio_accidente = 2019
+                    group by semana2019) as acc_2019 on (acc.dia_semana_accidente = semana2019)
+              join (select acc.dia_semana_accidente as semana2020, count(acc.numero) as num2020 
+                    from accidente_victima acc
+                    where acc.anio_accidente = 2020
+                    group by semana2020) as acc_2020 on (acc.dia_semana_accidente = semana2020)
+              join (select acc.dia_semana_accidente as semana2021, count(acc.numero) as num2021 
+                    from accidente_victima acc
+                    where acc.anio_accidente = 2021
+                    group by semana2021) as acc_2021 on (acc.dia_semana_accidente = semana2021)
+              group by dia, "2018", "2019", "2020", "2021"
+              order by total_acc desc""" 
 
